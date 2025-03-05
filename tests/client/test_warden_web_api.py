f66362ccbb5a415715fbb54a310e574ae62242ef
Olegs Capligins <ocapligins@ilionx.com>
2025-03-05 12:49:03 +0200

    client test fix and improvement

--- tests/client/test_warden_web_api.py
+++ tests/client/test_warden_web_api.py
@@ -1,5 +1,8 @@
 import json
+import os.path
 import unittest
+from pathlib import Path
+from tempfile import gettempdir
 from unittest.mock import patch
 from bit_vault_warden_client.client import WardenWebApi
 from bit_vault_warden_client.models import WardenConfiguration, Result, WardenCacheMode
@@ -8,22 +11,35 @@ from tests.models.login_data_builder import LoginDataBuilder
 
 
 dcs_retval = (b"test_credential", b"encryption_key", b"mac_key")
+cache_fname = 'cache.json'
+json_data = {
+    "profile": {"key": "fake_profile_key", "privateKey": "fake_private_key", "organizations": []},
+    "folders": [],
+    "collections": [],
+    "ciphers": [{"organizationId": "", "data": {"name": "test_credential", "password": "fake_password"}}]
+}
 
 
 class TestWardenWebApi(unittest.TestCase):
     def setUp(self):
+        Path(os.path.join(gettempdir(), cache_fname)).touch()
         self.client = WardenWebApi(
             WardenConfiguration(
                 url="https://example.com",
                 username="testuser",
                 password="testpassword",
-                cache_filename="cache.json",
-                cache_ttl=3600,
-                cache_mode=WardenCacheMode.AGGRESSIVE,
-                https_verify=True,
+                cache_filename=cache_fname,
+                https_verify=False,
+                https_proxy='https://localhost:6969',
+                http_proxy='http://localhost:6969',
+                cache_mode=WardenCacheMode.FALLBACK,
                 auth_data=LoginDataBuilder().build()
             )
         )
+
+    def tearDown(self):
+        Path(os.path.join(gettempdir(), cache_fname)).unlink(missing_ok=True)
+        del self.client
 
     @patch("bit_vault_warden_client.client.requests.Session.post")
     @patch("bit_vault_warden_client.client.requests.Session.get")
@@ -38,12 +54,7 @@ class TestWardenWebApi(unittest.TestCase):
             "access_token": "fake_token",
         }
 
-        mock_get.return_value.json.return_value = {
-            "profile": {"key": "fake_profile_key", "privateKey": "fake_private_key", "organizations": []},
-            "folders": [],
-            "collections": [],
-            "ciphers": [{"organizationId": "", "data": {"name": "test_credential", "password": "fake_password"}}]
-        }
+        mock_get.return_value.json.return_value = json_data
 
         result = self.client.fetch_credentials_from_remote("test_credential")
         self.assertIsInstance(result, Result)
@@ -54,12 +65,7 @@ class TestWardenWebApi(unittest.TestCase):
     def test_fetch_credentials_from_cache(self, _, mock_open):
         mock_open.return_value.__enter__.return_value.read.return_value = json.dumps({
             "KDFITERATIONS": 100000,
-            "DATA": {
-                "profile": {"key": "fake_profile_key", "privateKey": "fake_private_key", "organizations": []},
-                "folders": [],
-                "collections": [],
-                "ciphers": [{"organizationId": "", "data": {"name": "test_credential", "password": "fake_password"}}]
-            }
+            "DATA": json_data
         })
 
         result = self.client.fetch_credentials_from_cache("test_credential")
@@ -71,12 +77,7 @@ class TestWardenWebApi(unittest.TestCase):
     def test_fetch_credentials_from_cache_not_found(self, _, mock_open):
         mock_open.return_value.__enter__.return_value.read.return_value = json.dumps({
             "KDFITERATIONS": 100000,
-            "DATA": {
-                "profile": {"key": "fake_profile_key", "privateKey": "fake_private_key", "organizations": []},
-                "folders": [],
-                "collections": [],
-                "ciphers": [{"organizationId": "", "data": {"name": "test_credential", "password": "fake_password"}}]
-            }
+            "DATA": json_data
         })
 
         with self.assertRaises(CredentialsNotFoundError):
